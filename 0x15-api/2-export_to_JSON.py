3980c7adad913a91eff2fc0d419becb4a260f19e
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-14 23:11:07 +0100

    Update

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -18,9 +18,7 @@ if __name__ == '__main__':
     tasks = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
 
     # save to json
-    user_info = {
-        "{}".format(id): []
-    }
+    task_list = []
     with open("{}.json".format(id), "w") as f:
         for task in tasks:
             if task.get("userId") == id:
@@ -28,5 +26,5 @@ if __name__ == '__main__':
                 user["task"] = task.get("title")
                 user["completed"] = task.get("completed")
                 user["username"] = employee_name
-                user_info["{}". format(id)].append(user)
-        f.write(json.dumps(user_info))
+                task_list.append(user)
+        f.write(json.dumps({"{}".format(id): task_list}))
