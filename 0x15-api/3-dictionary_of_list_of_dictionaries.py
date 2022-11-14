593bd53440b0c18093de2b70af117043bf4e731b
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-14 19:33:14 +0100

    Update

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module returns information about all employees TODO list
+and saves it to json
+"""
+import json
+import requests
+
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/users/"
+
+    employees = requests.get(url).json()
+
+    tasks = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
+
+    # save to json
+    data = {}
+    with open("todo_all_employees.json".format(id), "w") as f:
+        for employee in employees:
+            id = employee.get("id")
+            user_info = {
+                "{}".format(id): []
+            }
+            for task in tasks:
+                if task.get("userId") == id:
+                    user = {}
+                    user["task"] = task.get("title")
+                    user["completed"] = task.get("completed")
+                    user["username"] = employee.get("username")
+                    user_info["{}". format(id)].append(user)
+            data.update(user_info)
+        f.write(json.dumps(data))
