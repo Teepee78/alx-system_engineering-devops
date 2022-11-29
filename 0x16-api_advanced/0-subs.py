e235dc4ff19a281dc45dc5c6e09e2771695bb115
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-29 22:03:07 +0100

    Update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,17 +7,17 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-	"""
-	Sends a query to reddit api for the number of subscribers
-	"""
-	api_url = "https://api.reddit.com/r/{}/about".format(subreddit)
-	headers = {
-		"User-Agent": "CustomClient/1.0",
-	}
+    """
+    Sends a query to reddit api for the number of subscribers
+    """
+    api_url = "https://api.reddit.com/r/{}/about".format(subreddit)
+    headers = {
+        "User-Agent": "CustomClient/1.0",
+    }
 
-	about = requests.get(api_url, headers=headers, allow_redirects=False)
-	if about.status_code != 200:
-		return 0
-	elif "subscribers" in about.json().get("data"):
-		return about.json().get("data").get("subscribers")
-	return 0
+    about = requests.get(api_url, headers=headers, allow_redirects=False)
+    if about.status_code != 200:
+        return 0
+    elif "subscribers" in about.json().get("data"):
+        return about.json().get("data").get("subscribers")
+    return 0
